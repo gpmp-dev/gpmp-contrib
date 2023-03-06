726d24d1e89a5bcb0843fdfa1ff7efa8194966f1
Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
2023-03-06 19:38:59 +0100

    wip

--- gpmpcontrib/models/stochasticmomodel.py
+++ gpmpcontrib/models/stochasticmomodel.py
@@ -8,8 +8,9 @@ License: GPLv3 (see LICENSE)
 """
 import jax
 import numpy as np
+import gpmp.numpy as gnp
 import gpmp as gp
-import jax.numpy as jnp
+from math import log
 
 
 def kernel_ii_or_tt(x, param, output_idx, pairwise=False):
@@ -27,22 +28,22 @@ def kernel_ii_or_tt(x, param, output_idx, pairwise=False):
     # parameters
     p = 2
     param_dim = param.shape[0]
-    sigma2 = jnp.exp(param[0])
-    invrho = jnp.exp(param[1:])
+    sigma2 = gnp.exp(param[0])
+    invrho = gnp.exp(param[1:])
     d = invrho.shape[0]
     noise_idx = d + output_idx
-    nugget = 10 * jnp.finfo(jnp.float64).eps
+    nugget = 10 * gnp.finfo(gnp.float64).eps
 
     if pairwise:
         # return a vector of covariances between predictands
-        K = sigma2 * jnp.ones((x.shape[0],)) + \
+        K = sigma2 * gnp.ones((x.shape[0],)) + \
             x[:, noise_idx] + nugget  # nx x 0
     else:
         # return a covariance matrix between observations
         xs = gp.kernel.scale(x[:, :d], invrho)
         K = gp.kernel.distance(xs, xs)  # nx x nx
         K = sigma2 * \
-            gp.kernel.maternp_kernel(p, K) + jnp.diag(x[:, noise_idx] + nugget)
+            gp.kernel.maternp_kernel(p, K) + gnp.diag(x[:, noise_idx] + nugget)
 
     return K
 
@@ -52,8 +53,8 @@ def kernel_it(x, y, param, pairwise=False):
     # parameters
     p = 2
     param_dim = param.shape[0]
-    sigma2 = jnp.exp(param[0])
-    invrho = jnp.exp(param[1:])
+    sigma2 = gnp.exp(param[0])
+    invrho = gnp.exp(param[1:])
     d = invrho.shape[0]
 
     xs = gp.kernel.scale(x[:, :d], invrho)
@@ -82,16 +83,20 @@ def build_kernel(output_idx):
 
 
 def constant_mean(x, param):
-    return jnp.ones((x.shape[0], 1))
+    return gnp.ones((x.shape[0], 1))
 
 
 def build_anisotropic_parameters_initial_guess(output_dim):
-
+    # since xi carries noise variance parameters
+    
     def anisotropic_parameters_initial_guess(model, xi, zi):
 
-        rho = jnp.std(xi, axis=0)[:output_dim]
+        xi_ = gnp.ensure_type(xi)
+        zi_ = gnp.ensure_type(zi)
 
-        covparam = jnp.concatenate((jnp.array([jnp.log(1.0)]), -jnp.log(rho)))
+        rho = gnp.std(xi_, axis=0)[:output_dim]
+
+        covparam = gnp.concatenate((gnp.array([log(1.0)]), -gnp.log(rho)))
 
         # n = xi.shape[0]
         # sigma2_GLS = 1 / n * model.norm_k_sqrd(
@@ -99,41 +104,42 @@ def build_anisotropic_parameters_initial_guess(output_dim):
         #     zi.reshape((-1, )),
         #     covparam
         # )
-        sigma2 = jnp.std(zi)
+        sigma2 = gnp.std(zi_)
 
-        return jnp.concatenate(
-            (jnp.array([jnp.log(sigma2)]), -jnp.log(rho)))
+        return gnp.concatenate(
+            (gnp.array([gnp.log(sigma2)]), -gnp.log(rho)))
 
     return anisotropic_parameters_initial_guess
 
 
 def negative_log_restricted_penalized_likelihood(model,
-                                                 xi,
-                                                 zi,
                                                  covparam,
                                                  mean_prior,
-                                                 invcov_prior):
+                                                 invcov_prior,
+                                                 xi,
+                                                 zi):
 
     delta = covparam - mean_prior
     penalization = 0.5 * delta.T @ (invcov_prior @ delta)
-    nlrel = model.negative_log_restricted_likelihood(xi, zi, covparam)
+    nlrel = model.negative_log_restricted_likelihood(covparam, xi, zi)
 
     return nlrel + penalization
 
 
-def build_make_remap_criterion(mean_prior, invcov_prior):
+def build_remap_criterion(model, mean_prior, invcov_prior):
 
-    def make_remap_criterion(model, xi, zi):
+    def remap_criterion(covparam, xi, zi):
+        nlrepl = negative_log_restricted_penalized_likelihood(
+            model,
+            covparam,
+            mean_prior,
+            invcov_prior,
+            xi,
+            zi
+        )     
+        return nlrepl
 
-        nlrepl = jax.jit(
-            lambda covparam: negative_log_restricted_penalized_likelihood
-            (model, xi, zi, covparam, mean_prior, invcov_prior)
-        )
-        dnlrepl = jax.grad(nlrepl)
-
-        return nlrepl, dnlrepl
-
-    return make_remap_criterion
+    return remap_criterion
 
 
 def build_models(output_dim):
@@ -151,13 +157,10 @@ def build_models(output_dim):
     mean_prior = np.array([0, -np.log(1 / 3), -np.log(1 / 3)])
     invcov_prior = np.diag([0, 1 / np.log(10/3)**2, 1 / np.log(10/3)**2])
 
-    # same remap criterion for all outputs
-    make_remap_criterion = build_make_remap_criterion(mean_prior, invcov_prior)
-
     for i in range(output_dim):
         covariance = build_kernel(i)
 
-        models[i]["model"] = gp.core.Model(constant_mean,
+        models[i]['model'] = gp.core.Model(constant_mean,
                                            covariance,
                                            None,
                                            None)
@@ -165,6 +168,10 @@ def build_models(output_dim):
             "parameters_initial_guess_procedure"
         ] = build_anisotropic_parameters_initial_guess(output_dim)
         
-        models[i]["make_selection_criterion"] = make_remap_criterion
+        models[i]["selection_criterion"] = build_remap_criterion(
+            models[i]['model'],
+            mean_prior,
+            invcov_prior
+        )
 
     return models
