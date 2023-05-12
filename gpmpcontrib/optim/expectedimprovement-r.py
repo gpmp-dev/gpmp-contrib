2da60b74399e9147c89ee000e97b47ab48ee15f8
Sébastien Petit <sebastien.julien.petit@gmail.com>
2023-05-12 13:43:08 +0200

    première version

--- gpmpcontrib/optim/expectedimprovement-r.py
+++ gpmpcontrib/optim/expectedimprovement-r.py
@@ -6,15 +6,55 @@ import gpmpcontrib.smc as gpsmc
 import numpy as np
 import gpmpcontrib.regp as regp
 
+t_getters = {
+    'Constant': lambda xi, zi, n_init, l: np.quantile(zi[:n_init], l),
+    'Concentration': lambda xi, zi, n_init, l: np.quantile(zi, l)
+}
+
 class ExpectedImprovementR(spred.SequentialPrediction):
+
+    def set_options(self, options):
+        default_options = {'n_smc': 1000}
+        default_options.update(options)
+        return default_options
 
     def predict(self, xt):
         """Prediction"""
+
         zpm = np.empty((xt.shape[0], self.output_dim))
         zpv = np.empty((xt.shape[0], self.output_dim))
         for i in range(self.output_dim):
+            t_i = self.get_t(self.xi, self.zi[:, i], self.n_init)
+
             _, (zpm[:, i], zpv[:, i]), _, _ = regp.predict(
-                self.models[i]['model'], self.xi, self.zi[:, i], xt, self.R[i]
+                self.models[i]['model'],
+                self.xi,
+                self.zi[:, i],
+                xt,
+                gnp.numpy.array([[t_i, gnp.numpy.inf]])
             )
 
         return zpm, zpv
+
+    def log_prob_excursion(self, x):
+        raise NotImplementedError
+
+    def compute_conditional_simulations(
+            self,
+            compute_zsim=True,
+            n_samplepaths=0,
+            xt='None',
+            type='intersection',
+            method='chol'
+    ):
+        raise NotImplementedError
+
+    def update_params(self):
+        raise NotImplementedError
+
+    def set_initial_design(self, xi, update_search_space=True):
+        self.n_init = xi.shape[0]
+        super().set_initial_design(xi, update_model=False, update_search_space=update_search_space)
+
+    def make_new_eval(self, xnew, update_search_space=True):
+        super().make_new_eval(xnew, update_model=False, update_search_space=update_search_space)
