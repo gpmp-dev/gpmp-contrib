798aa06062d6119a60617f05ef72698734c08b8b
Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
2024-10-01 11:04:14 +0200

    sequentialprediction.py: update documentation

--- gpmpcontrib/sequentialprediction.py
+++ gpmpcontrib/sequentialprediction.py
@@ -16,7 +16,7 @@ SequentialPrediction
 
 
 Author: Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
-Copyright (c) 2022-2023, CentraleSupelec
+Copyright (c) 2022-2024, CentraleSupelec
 License: GPLv3 (see LICENSE)
 
 """
@@ -27,8 +27,57 @@ from gpmpcontrib.models import Model
 
 class SequentialPrediction:
     def __init__(self, model):
+        """Facilitates sequential predictions with Gaussian process
+        (GP) models. This class supports the building, storing, and
+        managing of GP models and datasets, appending new evaluations,
+        making predictions, and simulating conditional sample paths.
+
+        Attributes
+        ----------
+        model : Model
+            An instance of the Model class or its subclasses, encapsulating the GP models.
+        xi : ndarray, optional
+            The input data points, updated dynamically as new evaluations are added.
+        zi : ndarray, optional
+            The corresponding output values for the input data points, reshaped as needed.
+        force_param_initial_guess : bool
+            Determines whether to force using an initial guess for model parameters during updates.
+        n_samplepaths : int, optional
+            Specifies the number of conditional sample paths to simulate.
+        xtsim : ndarray, optional
+            Points at which conditional sample paths are simulated.
+        xtsim_xi_ind : ndarray, optional
+            Indices of xi within xtsim, used for simulations.
+        xtsim_xt_ind : ndarray, optional
+            Indices of xt within xtsim, used for simulations.
+        zsim : ndarray, optional
+            Simulated unconditional sample paths.
+        zpsim : ndarray, optional
+            Simulated conditional sample paths.
+
+        Methods
+        -------
+        __init__(model)
+            Initializes the SequentialPrediction instance with a specified model.
+        set_data(xi, zi)
+            Sets the initial dataset for the model.
+        set_data_with_model_selection(xi, zi)
+            Sets the dataset and updates model parameters based on the new data.
+        set_new_eval(xnew, znew)
+            Adds a new evaluation to the dataset and adjusts the existing data arrays.
+        set_new_eval_with_model_selection(xnew, znew)
+            Adds a new evaluation to the dataset and updates model parameters accordingly.
+        update_params()
+            Updates the model parameters using the current dataset, optionally using an initial guess.
+        predict(xt, convert_out=True)
+            Makes predictions at the given points, optionally converting outputs.
+        compute_conditional_simulations(xt)
+            Generates conditional sample paths based on the current model state and data.
+
+        """
         if not isinstance(model, Model):
-            raise TypeError("model must be an instance of Model or its subclasses")
+            raise TypeError(
+                "model must be an instance of Model or its subclasses")
 
         self.model = model
         self.xi = None
@@ -55,7 +104,8 @@ class SequentialPrediction:
 
     def set_data(self, xi, zi):
         self.xi = gnp.asarray(xi)
-        self.zi = gnp.asarray(zi).reshape(zi.shape[0], -1)  # Ensure zi is a matrix
+        self.zi = gnp.asarray(zi).reshape(
+            zi.shape[0], -1)  # Ensure zi is a matrix
 
     def set_data_with_model_selection(self, xi, zi):
         self.set_data(xi, zi)
@@ -91,291 +141,3 @@ class SequentialPrediction:
         if self.zi.ndim == 1:
             self.zi = self.zi.reshape(-1, 1)
         return self.model.compute_conditional_simulations(self.xi, self.zi, xt)
-
-
-# class SequentialPrediction:
-#     """A class for managing and using Gaussian Process (GP) models for
-#     sequential predictions.
-
-#     This class is designed to simplify the process of working with GP
-#     models for sequential predictions. It allows for the building and
-#     storing of GP models, managing datasets, appending new
-#     evaluations, making predictions, and simulating conditional sample
-#     paths.
-
-#     Attributes
-#     ----------
-#     xi : ndarray
-#         The input data points.
-#     zi : ndarray
-#         The corresponding output values for the input data points.
-#     output_dim : int
-#         The dimension of the output space.
-#     models : list of dicts
-#         The list of GP models and their corresponding details.
-#     force_param_initial_guess : bool
-#         A flag to force using an initial guess for model parameters
-#         at each new evaluation.
-#     n_samplepaths : int
-#         Number of conditional sample paths to simulate.
-#     xtsim : ndarray
-#         Simulation points for conditional sample paths.
-#     xtsim_xi_ind : ndarray
-#         Indices for xi in xtsim.
-#     xtsim_xt_ind : ndarray
-#         Indices for xt in xtsim.
-#     zsim : ndarray
-#         Unconditional sample paths.
-#     zpsim : ndarray
-#         Conditional sample paths.
-
-#     Methods
-#     -------
-#     __init__(output_dim=1, models=None)
-#         Initializes the SequentialPrediction instance.
-#     build_models(models=None)
-#         Builds GP models based on the provided models or default settings.
-#     set_data(xi, zi)
-#         Sets the dataset for the model.
-#     set_data_with_model_selection(xi, zi)
-#         Sets the dataset and updates model parameters.
-#     set_new_eval(xnew, znew)
-#         Adds a new evaluation to the dataset.
-#     set_new_eval_with_model_selection(xnew, znew)
-#         Adds a new evaluation and updates model parameters.
-#     update_params()
-#         Updates the parameters of the GP models.
-#     predict(xt)
-#         Makes predictions at the given points.
-#     compute_conditional_simulations(...)
-#         Generates conditional sample paths based on the current model and data.
-
-#     """
-
-#     def __init__(self, output_dim=1, models=None):
-#         """Initializes the SequentialPrediction instance with specified output dimension and models."""
-
-#         # dataset
-#         # xi : ndarray(ni, d)
-#         # zi : ndarray(ni, output_dim)
-#         self.xi = None
-#         self.zi = None
-
-#         # initialize models
-#         self.output_dim = output_dim
-#         self.build_models(models)
-
-#         # parameter selection
-#         self.force_param_initial_guess = False
-
-#         # unconditional & conditional simulations
-#         self.n_samplepaths = None
-#         self.xtsim = None
-#         self.xtsim_xi_ind = None
-#         self.xtsim_xt_ind = None
-#         self.zsim = None
-#         self.zpsim = None
-
-#     def __repr__(self):
-#         info = {
-#             "input_dim": self.xi.shape[1],
-#             "output_dim": self.zi.shape[1],
-#             "data_size": self.xi.shape[0],
-#         }
-#         return str(info)
-
-#     def build_models(self, models):
-#         """Builds GP models based on the provided models or default settings."""
-#         if models is None:
-#             self.models = [
-#                 {
-#                     "name": "default",
-#                     "model": None,
-#                     "parameters_initial_guess_procedure": None,
-#                     "parameters_initial_guess_procedure_builder": None,
-#                     "parameters_initial_guess_procedure_builder_args": None,
-#                     "selection_criterion": None,
-#                     "selection_criterion_builder": None,
-#                     "selection_criterion_builder_args": None,
-#                     "info": None,
-#                 }
-#                 for i in range(self.output_dim)
-#             ]
-
-#             for i in range(self.output_dim):
-#                 self.models[i]["model"] = gp.core.Model(
-#                     self.constant_mean, self.default_covariance, None, None
-#                 )
-#                 self.models[i][
-#                     "parameters_initial_guess_procedure"
-#                 ] = gp.kernel.anisotropic_parameters_initial_guess
-#                 self.models[i]["selection_criterion"] = self.models[i][
-#                     "model"
-#                 ].negative_log_restricted_likelihood
-#         else:
-#             self.models = models
-
-#     def constant_mean(self, x, param):
-#         return gnp.ones((x.shape[0], 1))
-
-#     def default_covariance(self, x, y, covparam, pairwise=False):
-#         p = 2
-#         return gp.kernel.maternp_covariance(x, y, p, covparam, pairwise)
-
-#     def set_data(self, xi, zi):
-#         self.xi = xi
-#         self.zi = zi.reshape(zi.shape[0], -1)  # self.zi is a matrix
-#         # even if zi is a vector
-
-#     def set_data_with_model_selection(self, xi, zi):
-#         self.set_data(xi, zi)
-#         self.update_params()
-
-#     def set_new_eval(self, xnew, znew):
-#         self.xi = np.vstack((self.xi, xnew))
-#         self.zi = np.vstack((self.zi.reshape(self.zi.shape[0], -1), znew))
-
-#     def set_new_eval_with_model_selection(self, xnew, znew):
-#         self.set_new_eval(xnew, znew)
-#         self.update_params()
-
-#     def parameters_initial_guess(self, model_index):
-#         model = self.models[model_index]
-
-#         if callable(model["parameters_initial_guess_procedure"]):
-#             initial_guess_procedure = model["parameters_initial_guess_procedure"]
-#         elif callable(model["parameters_initial_guess_procedure_builder"]):
-#             builder = model["parameters_initial_guess_procedure_builder"]
-#             initial_guess_procedure = builder(
-#                 model["parameters_initial_guess_builder_args"]
-#             )
-#         else:
-#             raise ValueError("Invalid initial guess procedure setting")
-
-#         covparam0 = initial_guess_procedure(
-#             self.models[i]["model"], self.xi, self.zi[:, i]
-#         )
-
-#         return gnp.asarray(covparam0)
-
-#     def make_selection_criterion_with_gradient(self):
-#         crit, dcrit = gp.kernel.make_selection_criterion_with_gradient(
-#             self.models[i]["selection_criterion"], self.xi, self.zi[:, i]
-#         )
-#         return crit, dcrit
-
-#     def update_params(self):
-#         """Parameter selection"""
-
-#         for i in range(self.output_dim):
-#             tic = time.time()
-
-#             if (
-#                 self.models[i]["model"].covparam is None
-#                 or self.force_param_initial_guess
-#             ):
-#                 covparam0 = self.parameters_initial_guess(i)
-#             else:
-#                 covparam0 = self.models[i]["model"].covparam
-
-#             covparam0 = gnp.asarray(covparam0)
-
-#             crit, dcrit = self.make_selection_criterion_with_gradient(i)
-
-#             covparam, info = gp.kernel.autoselect_parameters(
-#                 covparam0, crit, dcrit, silent=True, info=True
-#             )
-
-#             self.models[i]["model"].covparam = gnp.asarray(covparam)
-
-#             self.models[i]["info"] = info
-
-#             self.models[i]["info"]["covparam0"] = covparam0
-#             self.models[i]["info"]["covparam"] = covparam
-#             self.models[i]["info"]["selection_criterion"] = crit
-#             self.models[i]["info"]["time"] = time.time() - tic
-
-#     def predict(self, xt):
-#         """Prediction"""
-#         zpm = np.empty((xt.shape[0], self.output_dim))
-#         zpv = np.empty((xt.shape[0], self.output_dim))
-#         for i in range(self.output_dim):
-#             zpm[:, i], zpv[:, i] = self.models[i]["model"].predict(
-#                 self.xi, self.zi[:, i], xt
-#             )
-#         return zpm, zpv
-
-#     def compute_conditional_simulations(
-#         self,
-#         compute_zsim=True,
-#         n_samplepaths=0,
-#         xt="None",
-#         type="intersection",
-#         method="chol",
-#     ):
-#         """Generate conditional sample paths
-
-#         Parameters
-#         ----------
-#         xt : ndarray(nt, d)
-#             Simulation points
-#         n_samplepaths : int
-#             Number of sample paths
-#         type : 'intersection', 'disjoint'
-#             If type is 'intersection', xi and xt may have a non-empty
-#             intersection (as when xi is a subset of xt).
-#             If type is 'disjoint', xi and xt must be disjoint
-#         compute_zsim : compute zsim if True or else use self.zsim
-#         method : method to draw unconditional sample paths
-
-#         """
-#         if compute_zsim:
-#             # initialize self.xtsim and unconditional sample paths on self.xtsim
-#             self.n_samplepaths = n_samplepaths
-#             ni = self.xi.shape[0]
-#             nt = xt.shape[0]
-
-#             self.xtsim = np.vstack((self.xi, xt))
-#             if type == "intersection":
-#                 self.xtsim, indices = np.unique(self.xtsim, return_inverse=True, axis=0)
-#                 self.xtsim_xi_ind = indices[0:ni]
-#                 self.xtsim_xt_ind = indices[ni : (ni + nt)]
-#                 n = self.xtsim.shape[0]
-#             elif type == "disjoint":
-#                 self.xtsim_xi_ind = np.arange(ni)
-#                 self.xtsim_xt_ind = np.arange(nt) + ni
-#                 n = ni + nt
-
-#             # sample paths on xtsim
-#             self.zsim = np.empty((n, n_samplepaths, self.output_dim))
-
-#             for i in range(self.output_dim):
-#                 self.zsim[:, :, i] = gnp.to_np(
-#                     self.models[i]["model"].sample_paths(
-#                         self.xtsim, self.n_samplepaths, method="svd"
-#                     )
-#                 )
-
-#         # conditional sample paths
-#         self.zpsim = np.empty((nt, n_samplepaths, self.output_dim))
-
-#         for i in range(self.output_dim):
-#             zpm, zpv, lambda_t = self.models[i]["model"].predict(
-#                 self.xi,
-#                 self.zi[:, i],
-#                 self.xtsim[self.xtsim_xt_ind],
-#                 return_lambdas=True,
-#             )
-#             self.zpsim[:, :, i] = gnp.to_np(
-#                 self.models[i]["model"].conditional_sample_paths(
-#                     self.zsim[:, :, i],
-#                     self.xtsim_xi_ind,
-#                     self.zi[:, i],
-#                     self.xtsim_xt_ind,
-#                     lambda_t,
-#                 )
-#             )
-
-#         if self.output_dim == 1:
-#             # drop last dimension
-#             self.zpsim = self.zpsim.reshape((self.zpsim.shape[0], self.zpsim.shape[1]))
