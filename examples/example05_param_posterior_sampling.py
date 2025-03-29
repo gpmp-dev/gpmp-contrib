d9be8020f698ec8cc098192bfa3a4e6f5b8c63f1
Emmanuel Vazquez <emmanuel.vazquez@centralesupelec.fr>
2025-03-29 10:55:13 +0100

    example05: update

--- examples/example05_param_posterior_sampling.py
+++ examples/example05_param_posterior_sampling.py
@@ -1,36 +1,33 @@
+from scipy.io import savemat
 import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 import gpmp as gp
-import gpmp.num as gnp
 import gpmpcontrib as gpc
 
 
 warnings.simplefilter("error", RuntimeWarning)
 
 # -- definition of a mono-objective problem
-dim = 6
-problem = gpc.ComputerExperiment(
-    dim,  # dim of search space
-    [[0] * dim, [1] * dim],  # search box
-    single_function=gp.misc.testfunctions.hartmann6,  # test function
-)
+
+problem = gpc.test_problems.hartman6
+# problem = gpc.test_problems.braninPlus
+dim = problem.input_dim
 
 # -- create initial dataset
 
-nt = 2000
+nt = 1000
 xt = gp.misc.designs.maximinlhs(problem.input_dim, nt, problem.input_box)
 zt = problem(xt)
 
-ni = 200
-ind = np.arange(ni)
-xi = xt[ind]
-zi = zt[ind]
+ni = 5 * dim
+xi = gp.misc.designs.maximinlhs(problem.input_dim, ni, problem.input_box)
+zi = problem(xi)
 
 
 # -- initialize a model and make predictions
 model = gpc.Model_ConstantMean_Maternp_REMAP(
-    "GP6d",
+    "GPnd",
     output_dim=problem.output_dim,
     mean_params={"type": "constant"},
     covariance_params={"p": 2},
@@ -42,14 +39,25 @@ model.diagnosis(xi, zi)
 zloom, zloov, eloo = model.loo(xi, zi)
 gp.misc.plotutils.plot_loo(zi.reshape(-1), zloom.reshape(-1), zloov.reshape(-1))
 
+random_init = True
+init_box = [-8, -1]
+
 res = model.sample_parameters(
-    n_steps_total=10_000,
+    n_steps_total=5_000,
     burnin_period=4_000,
-    n_chains=2,
+    n_chains=6,
     show_progress=True,
     silent=False,
+    random_init=random_init,
+    init_box=init_box,
 )
 
 gp.misc.plotutils.crosssections(
-    model, xi, zi, problem.input_box, ind_i=[0, 1], ind_dim=list(range(6))
+    model, xi, zi, problem.input_box, ind_i=[0, 1], ind_dim=list(range(dim))
 )
+
+gp.misc.modeldiagnosis.plot_selection_criterion_crossections(
+    model[0].model, model[0].info, delta=5, n_points=200
+)
+
+savemat("data_xizi.mat", {"xi": xi, "zi": zi})
